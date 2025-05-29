e9a88e6285a5f3d27cdc546fec12aa8557a77c1e
Latanza <latanzaakbarfadilah@gmail.com>
2025-05-30 01:49:20 +0700

    updated

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 const PORT = 3000;
 
-app.use(cors({ origin: 'http://localhost:7700' }));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
