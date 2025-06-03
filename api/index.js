673902bb82728506c4f753e4c496433124fcd286
Latanza <latanzaakbarfadilah@gmail.com>
2025-06-03 22:04:45 +0700

    fix bug swagger

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use(
     cors({
         origin: allowedOrigin,
         methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type: multipart/form-data", "Authorization"],
+        allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true
     })
 );
